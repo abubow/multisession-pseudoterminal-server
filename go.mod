441a499f93e420a52c7d1bc2abb1013c55e41daa
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2025-01-09 18:41:40 +0500

    bash test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module multisession-pseudoterminal-server
 
 go 1.23.3
 
-require github.com/creack/pty v1.1.24 // indirect
+require (
+	github.com/creack/pty v1.1.24 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -4,22 +4,26 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/creack/pty"
 )
 
 func main() {
-	c := exec.Command("grep", "--color=auto", "bar")
+	c := exec.Command("bash")
 	f, err := pty.Start(c)
 	if err != nil {
 		panic(err)
 	}
 
 	go func() {
-		f.Write([]byte("foo\n"))
-		f.Write([]byte("bar\n"))
-		f.Write([]byte("baz\n"))
-		f.Write([]byte{4}) // EOT
+		f.Write([]byte("ls\n"))
+		time.Sleep(1000 * time.Millisecond)
+		f.Write([]byte("whoami\n"))
+		time.Sleep(1000 * time.Millisecond)
+		f.Write([]byte("sleep 3\n"))
+		f.Write([]byte{4})
+		time.Sleep(1000 * time.Millisecond)
 	}()
 	io.Copy(os.Stdout, f)
 }
